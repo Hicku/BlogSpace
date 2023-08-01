2cbc92610eb195061d2ee6445ef526ac58f91a72
Quas <aaron.hickman201@gmail.com>
2023-08-01 16:55:28 +0100

    update

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
             document.getElementById("blog-list").innerHTML = html
         }
     })
+
+    let newPost = document.getElementById('new-post')
+    newPost.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const postTitle = document.getElementById('post-title').value
+        const postBody = document.getElementById('post-body').value
+
+        const data = {
+            title: postTitle, 
+            body: postBody
+        }
+
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        fetch('https://apis.scrimba.com/jsonplaceholder/posts', options)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    })
