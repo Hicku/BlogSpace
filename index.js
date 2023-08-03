cc4c6b30a0c59101b9960880546657fbbdb5f917
Quas <aaron.hickman201@gmail.com>
2023-08-03 14:00:26 +0100

    update

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
+//"https://apis.scrimba.com/jsonplaceholder/posts"
 
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+let postArray = []
+let postBody = document.getElementById('post-body')
+let postTitle = document.getElementById('post-title')
+
+let renderPosts = () => {
+    let html = '';
+    for(let post of postArray) {
+        html +=
+        `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        `
+    }
+    document.getElementById('blog-list').innerHTML = html
+}
+
+fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(res => res.json())
     .then(data => {
-        let postArr = data.slice(0, 5)
-        let html = ""
-
-        for(post of postArr) {
-            html += `
-                <h3>${post.title}<h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-            document.getElementById("blog-list").innerHTML = html
-        }
+        postArray = data.slice(0, 5)
+        renderPosts()
     })
 
-    let newPost = document.getElementById('new-post')
-    newPost.addEventListener("submit", (e) => {
-        e.preventDefault();
+    document.getElementById('new-post').addEventListener('submit', (e) => {
+        e.preventDefault()
 
-        const postTitle = document.getElementById('post-title').value
-        const postBody = document.getElementById('post-body').value
-
-        const data = {
-            title: postTitle, 
-            body: postBody
+        let postData = {
+            title: postTitle.value,
+            body: postBody.value
         }
 
-        const options = {
+        let options = {
             method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json"
-            }
+            body: JSON.stringify(postData),
+            headers: {'Content-Type' : 'application/json'}
+
         }
 
         fetch('https://apis.scrimba.com/jsonplaceholder/posts', options)
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                postArray.unshift(postData)
+                renderPosts()
             })
+            postTitle.value = ''
+            postBody.value = ''
     })
